0ece30b4be781535143a6069d711cb9637c06121
Rishabh Dubey <rishabhdubey006@gmail.com>
2024-09-30 17:36:08 +0530

    random generator

--- javascript_intervie/index.js
+++ javascript_intervie/index.js
@@ -3,17 +3,20 @@
 // console.log(intArray)
 
 //genrate alpha numeric captch using javascript
-function randomGen(){
-    const char="1234567890QWERTYUIOPLKJHGFDSAZXCVBNMqwertsyhauiokhnbvcxzaml"
+// function randomGen(){
+//     const char="1234567890QWERTYUIOPLKJHGFDSAZXCVBNMqwertsyhauiokhnbvcxzaml"
 
 
-    let charlen = 7 ;
-    let result = "";
+//     let charlen = 7 ;
+//     let result = "";
 
-    for (i =0; i<charlen; i++){
-      let randomGenn = Math.floor(Math.random() * char.length)  
+//     for (i =0; i<charlen; i++){
+//       let randomGenn = Math.floor(Math.random() * char.length)  
           
-         result +=char.substring(randomGenn , randomGenn+1);
-    }
-    document.getElementById("randomchar").innerHTML= result;
-}
+//          result +=char.substring(randomGenn , randomGenn+1);
+//     }
+//     document.getElementById("randomchar").innerHTML= result;
+// }
+
+
+//find the last element of an array without using length property
