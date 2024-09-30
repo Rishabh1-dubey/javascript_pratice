89f7abb0ff1d6b7050f26c79955d889bb2e7aca9
Rishabh Dubey <rishabhdubey006@gmail.com>
2024-09-30 17:35:51 +0530

    random generator

--- javascript_intervie/index.js
+++ javascript_intervie/index.js
@@ -1,3 +1,19 @@
-const intArray=[1,3,4]
-intArray[50]=50;
-console.log(intArray)
+// const intArray=[1,3,4]
+// intArray[50]=50;
+// console.log(intArray)
+
+//genrate alpha numeric captch using javascript
+function randomGen(){
+    const char="1234567890QWERTYUIOPLKJHGFDSAZXCVBNMqwertsyhauiokhnbvcxzaml"
+
+
+    let charlen = 7 ;
+    let result = "";
+
+    for (i =0; i<charlen; i++){
+      let randomGenn = Math.floor(Math.random() * char.length)  
+          
+         result +=char.substring(randomGenn , randomGenn+1);
+    }
+    document.getElementById("randomchar").innerHTML= result;
+}
